3a93e0ccea4130d9ab57de0d7be28fcf6dc08a4e
Michał <83660460+MagicianPL@users.noreply.github.com>
2023-10-23 09:13:29 +0200

    add custom form in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryContentComponent } from './components/categoryContent/category-content/category-content.component';
+import { CustomFlashcardFormComponent } from './components/custom-flashcard-form/custom-flashcard-form.component';
 import { InfoComponent } from './components/info/info.component';
 
 const routes: Routes = [
   { path: '', component: InfoComponent },
   { path: ':category', component: CategoryContentComponent },
+  { path: ':category', component: CategoryContentComponent },
+  { path: ':favorites/custom', component: CustomFlashcardFormComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' } // Wildcard route for redirection
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,10 @@ import { AppComponent } from './app.component';
 import { FlashcardComponent } from './components/flashcard/flashcard.component';
 import { HeaderComponent } from './components/header/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { MainPageComponent } from './pages/mainPage/main-page/main-page.component';
 import { InfoComponent } from './components/info/info.component';
 import { CategoryContentComponent } from './components/categoryContent/category-content/category-content.component';
 import { FooterComponent } from './components/footer/footer/footer.component';
+import { CustomFlashcardFormComponent } from './components/custom-flashcard-form/custom-flashcard-form.component';
 
 
 @NgModule({
@@ -20,10 +20,10 @@ import { FooterComponent } from './components/footer/footer/footer.component';
     FlashcardComponent,
     HeaderComponent,
     NavigationComponent,
-    MainPageComponent,
     InfoComponent,
     CategoryContentComponent,
-    FooterComponent
+    FooterComponent,
+    CustomFlashcardFormComponent
   ],
   imports: [
     BrowserModule,
