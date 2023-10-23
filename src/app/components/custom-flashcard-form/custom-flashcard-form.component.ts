f9dde449eb9701a2f93022268f1d697becd019f9
Michał <83660460+MagicianPL@users.noreply.github.com>
2023-10-23 09:12:51 +0200

    add custom form in progress

---
+++ src/app/components/custom-flashcard-form/custom-flashcard-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-flashcard-form',
+  templateUrl: './custom-flashcard-form.component.html',
+  styleUrls: ['./custom-flashcard-form.component.css']
+})
+export class CustomFlashcardFormComponent {
+
+}
